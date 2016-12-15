60df9e02cb3014c81510e33c1ac3ca2eae376f93
extraErik <erygg01@live.com>
2016-12-15 16:09:26 -0600

    Fetch todos from firebase on app start

--- app/components/TodoList.jsx
+++ app/components/TodoList.jsx
@@ -6,13 +6,17 @@ var TodoAPI = require('TodoAPI');
 export var TodoList = React.createClass({
     render: function () {
         var {todos, showCompleted, searchText} = this.props;
+        var incompleteTodos = todos.filter((todo) => {
+            return !todo.completed;
+        });
         var renderTodos = () => {
-            if (todos.length === 0) {
+            var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+            if (filteredTodos.length === 0) {
                 return (
                     <p className="container__message">Nothing To Do</p>
                 )
             } else {
-                return TodoAPI.filterTodos(todos, showCompleted, searchText).map((todo) => {
+                return filteredTodos.map((todo) => {
                     return (
                         <Todo key={todo.id} {...todo} />
                     );
